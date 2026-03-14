a21252d553a2227bba6987b2efbb85fdfeeb69b2
moUA2007 <130896266+moUA2007@users.noreply.github.com>
2026-03-14 17:56:50 +0200

    fix: use absolute callback URL for Google OAuth

--- src/config/passport.js
+++ src/config/passport.js
@@ -8,7 +8,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: 'https://ecommerce-api-psi-three.vercel.app/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ email: profile.emails[0].value });
